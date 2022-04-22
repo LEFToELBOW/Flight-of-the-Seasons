df753f88203606d0a0664b6bd527ad3f18219c98
Patrick Del Rio <patdelrio05@gmail.com>
2022-04-22 11:57:22 -0400

    proximity code

--- New Unity Project/Assets/Scripts/Check.cs
+++ New Unity Project/Assets/Scripts/Check.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Check : MonoBehaviour
-{
-    private bool far, med, close;
-    private float Fdistance = 200f;
-    private float Mdistance = 50f;
-    private float Cdistance = 20f;
-    public GameObject key;
+{ 
+    private float Fdistance = 700f;
+    private float Mdistance = 350f;
+    private float Cdistance = 200f;
+    public GameObject key, close, med, far, blank, canvas;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +18,30 @@ public class Check : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(key.transform.position, transform.position) <= Cdistance)
+        if (Input.GetKeyDown("e"))
         {
-
+            
+            if (Vector3.Distance(key.transform.position, transform.position) <= Cdistance)
+            {
+                Instantiate(close, canvas.transform);
+                Destroy(close, 1.0f);
+            }
+            else if (Vector3.Distance(key.transform.position, transform.position) <= Mdistance)
+            {
+                Instantiate(med, canvas.transform);
+                Destroy(med, 1.0f);
+            }
+            else if (Vector3.Distance(key.transform.position, transform.position) <= Fdistance)
+            {
+                Instantiate(far, canvas.transform);
+                Destroy(far, 1.0f);
+            }
+            else
+            {
+                Instantiate(blank, canvas.transform);
+                Destroy(blank, 1.0f);
+            }
         }
-        else if(Vector3.Distance(key.transform.position, transform.position) <= Mdistance)
-        {
-
-        }
-        else if(Vector3.Distance(key.transform.position, transform.position) <= Fdistance)
-        {
-
-        }
-        else
-        {
-
-        }
+        
     }
 }
