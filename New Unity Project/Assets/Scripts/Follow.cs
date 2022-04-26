83fe9eb2780c960650431ee9dcdfdb4223c949e3
phishstew <vkunnummal1@gmail.com>
2022-04-25 22:19:33 -0400

    Sentient bushes
    They do be following the player

---
+++ New Unity Project/Assets/Scripts/Follow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform targetTransform;
+    private float speed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(targetTransform.position);
+        if ((transform.position - targetTransform.position).magnitude > 0.1f)
+        {
+            transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+        }
+    }
+}
